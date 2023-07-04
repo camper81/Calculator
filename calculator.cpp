1215f8f36614f11cd34c733e82bbc12df01e647b
Max <mbelikyan@telematika.com>
2023-07-04 23:18:51 +0300

    Реализация паттерна State для математических операций

--- calculator.cpp
+++ calculator.cpp
@@ -6,10 +6,10 @@ Calculator::Calculator(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
-    ui->setupUi(this);
-
+    ui->setupUi(this);    
+    m_state = new Initial();
     for(auto&& child : ui->gOperaions->children()) {
-        connect(qobject_cast<QPushButton*>(child), &QPushButton::clicked, this, &Calculator::onOperationClick);
+        connect(qobject_cast<QPushButton*>(child), &QPushButton::clicked, this, &Calculator::onOperatorClick);
     }
 
     for(auto child : ui->gNumpad->children()) {
@@ -17,15 +17,105 @@ Calculator::Calculator(QWidget *parent)
     }
 }
 
-
-void Calculator::onNumpadClick() {
-    if(checkTextField()) {
-
+void Calculator::onOperatorClick() {
+    QString text = qobject_cast<QPushButton*>(sender())->text();
+    if(text == "+") {
+        m_state->setState(this, new Addition());
+    } else if (text == "-") {
+        m_state->setState(this, new Subtraction());
+    } else if (text == "=") {
+        ui->eMain->setText(QString::number(m_prev_result));
+        m_state->getResult(this);
     }
 }
 
+void Calculator::onNumpadClick() {    
+    if(!checkTextField(m_text))
+        return;
+
+    QString sym = qobject_cast<QPushButton*>(sender())->text();
+
+    if(hasComma(m_text) && sym == ".")
+        return;
+
+    m_text.append(sym);
+    ui->eMain->setText(m_text);
+}
+
+float Calculator::getNumberFromTextField() {
+    return m_text.toFloat();
+}
+
+float Calculator::getPreviousResult() {
+    return m_prev_result;
+}
+
+void Calculator::setPreviousResult(float prev_result) {
+    m_prev_result = prev_result;
+}
 
 Calculator::~Calculator()
 {
     delete ui;
+}
+
+void Calculator::setTextField(QString text) {
+    ui->eMain->setText(text);
+}
+
+void Calculator::resetTextField() {
+    m_text.clear();
+}
+
+bool Calculator::checkTextField(QString text)
+{
+    if(text.size() > MAX_TEXT_FIELD)
+        return false;
+
+    return true;
+}
+
+bool Calculator::hasComma(QString text)
+{
+    return text.contains('.');
+}
+
+void Initial::setState(Calculator* calculator, State* new_state) {
+    calculator->setPreviousResult(calculator->getNumberFromTextField());
+    calculator->setState(new_state);
+    calculator->resetTextField();
+    delete this;
+}
+
+void Initial::getResult(Calculator *calculator) {
+    calculator->resetTextField();
+}
+
+void Addition::getResult(Calculator *calculator) {
+    float result = calculator->getPreviousResult() + calculator->getNumberFromTextField();
+    calculator->setPreviousResult(result);
+    calculator->setTextField(QString::number(result));
+}
+
+void Subtraction::getResult(Calculator *calculator) {
+    float result = calculator->getPreviousResult() - calculator->getNumberFromTextField();
+    calculator->setTextField(QString::number(result));
+    calculator->setState(new Initial());
+    delete this;
+}
+
+void Addition::setState(Calculator* calculator, State* new_state) {
+    float result = calculator->getPreviousResult() + calculator->getNumberFromTextField();
+    calculator->setPreviousResult(result);
+    calculator->setState(new_state);
+    calculator->resetTextField();
+    delete this;
+}
+
+void Subtraction::setState(Calculator* calculator, State* new_state) {
+    float result = calculator->getPreviousResult() - calculator->getNumberFromTextField();
+    calculator->setPreviousResult(result);
+    calculator->setState(new_state);
+    calculator->resetTextField();
+    delete this;
 }
--- calculator.h
+++ calculator.h
@@ -7,32 +7,81 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
 
+class Calculator;
+
 enum class Operations{
     unknown, add, sub, mul, div
+};
+
+class State {
+public:
+    virtual void setState(Calculator* obj, State* new_state) = 0;
+    virtual void getResult(Calculator* obj) = 0;
+    virtual ~State() {
+
+    }
+};
+
+class Addition : public State {
+public:
+    void setState(Calculator* obj, State* new_state);
+    void getResult(Calculator* obj);
+};
+
+class Subtraction : public State {
+public:
+    void setState(Calculator* obj, State* new_state);
+    void getResult(Calculator* obj);
+};
+
+class Multiplication : public State {
+public:
+    void setState(Calculator* obj, State* new_state);
+    void getResult(Calculator* obj);
+};
+
+class Devision: public State {
+public:
+    void setState(Calculator* obj, State* new_state);
+    void getResult(Calculator* obj);
+};
+
+class Initial: public State {
+public:
+    void setState(Calculator* obj, State* new_state);
+    void getResult(Calculator* obj);
 };
 
 class Calculator : public QWidget
 {
     Q_OBJECT
-    Q_ENUM(Operations)
-    Q_PROPERTY(Operations m_currentOperation READ currentOperation WRITE setCurrentOperation NOTIFY onChangeOperation)
 
 public:
     Calculator(QWidget *parent = nullptr);
     ~Calculator();
 
-    void completeOperation();
-
-    void resetTextField();
-    bool checkTextField();
-
-    void onOperationClick();
     void onNumpadClick();
+    void onOperatorClick();
+    void setState(State* state) {
+        m_state = state;
+    }
+    float getNumberFromTextField();
+    float getPreviousResult();
+    void setPreviousResult(float);
+    void resetTextField();
+    void setTextField(QString);
 signals:
     void onChangeOperation();
 
 private:
+    const int MAX_TEXT_FIELD = 12;
+
     Ui::Widget *ui;
-    Operations m_currentOperation;
+
+    State* m_state;
+    QString m_text;
+    bool checkTextField(QString text);
+    bool hasComma(QString text);
+    float m_prev_result;
 };
 #endif // CALCULATOR_H
